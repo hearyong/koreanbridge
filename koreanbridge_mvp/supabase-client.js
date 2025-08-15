3e8f408a7b507391f1d886fcc1fabda17f7b0660
hearyon <mhr730728@gmail.com>
2025-08-15 10:54:26 +0900

    v.1

---
+++ koreanbridge_mvp/supabase-client.js
@@ -0,0 +1,6 @@
+// supabase-client.js
+
+const supabaseUrl = 'https://npcrxldifamqgtafjhgw.supabase.co'; // 여기에 복사한 Project URL을 붙여넣으세요.
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wY3J4bGRpZmFtcWd0YWZqaGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxOTYxNTYsImV4cCI6MjA3MDc3MjE1Nn0.uU1gsGPNyZgl4lHVebtKvwk4e52s2lj5wYPkkIq_b_M'; // 여기에 복사한 Anon Public Key를 붙여넣으세요.
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
